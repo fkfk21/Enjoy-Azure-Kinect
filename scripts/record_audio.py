56c32204bb7b95385ed18154326b54f1284cf0d1
Kenji Fukuda <fk.kenji.21@gmail.com>
2023-10-27 20:50:58 +0900

    add audio test code

---
+++ scripts/record_audio.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sounddevice as sd
+from scipy.io.wavfile import write
+from isort import file
+from tools import get_device_number
+
+# 録音の設定
+fs = 48000  # サンプリング周波数
+duration = 5  # 録音時間（秒）
+channels = 7
+device_num = get_device_number("Azure Kinect") # マイクロフォンアレイのデバイス番号
+
+# マイクロフォンアレイからの録音
+print("録音開始...")
+audio_data = sd.rec(int(duration * fs), samplerate=fs, device=device_num,
+                    channels=channels, dtype='float64')
+sd.wait()  # 録音が終了するまで待つ
+print("録音終了")
+
+# 各マイクロフォンのデータを別々のファイルに保存
+# for i in range(audio_data.shape[1]):
+#     filename = f"mic_{i+1}.wav"
+#     write(filename, fs, audio_data[:, i])
+#     print(f"{filename}に保存しました")
+
+# マイクロフォンアレイのデータをファイルに保存
+filename = "mic_array.wav"
+write(filename, fs, audio_data)
+print(f"{filename}に保存しました")
--- scripts/test_microphone_array.py
+++ scripts/test_microphone_array.py
@@ -1,19 +1,40 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import sounddevice as sd
-from scipy.io.wavfile import write
+from tools import get_device_number
 
-# 録音の設定
-fs = 44100  # サンプリング周波数
-duration = 5  # 録音時間（秒）
 
-# マイクロフォンアレイからの録音
-print("録音開始...")
-audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=4, dtype='float64')
-sd.wait()  # 録音が終了するまで待つ
-print("録音終了")
+# サンプリング周波数とバッファサイズの設定
+fs = 48000
+buffer_size = 2048
+device_num = get_device_number("Azure Kinect") # マイクロフォンアレイのデバイス番号
+channels = 7 # マイクロフォンアレイの数
 
-# 各マイクロフォンのデータを別々のファイルに保存
-for i in range(audio_data.shape[1]):
-    filename = f"mic_{i+1}.wav"
-    write(filename, fs, audio_data[:, i])
-    print(f"{filename}に保存しました")
+# プロットをインタラクティブモードで表示
+fig, ax = plt.subplots()
+x = np.fft.rfftfreq(buffer_size, d=1/fs)
+y = np.zeros_like(x)
+line, = ax.plot(x, y)
+ax.set_ylim(0, 20.0)
+ax.set_xlim(0, fs/2)
+# ax.set_xlim(0, 10000)
+ax.set_xlabel('Frequency [Hz]')
+ax.set_ylabel('Magnitude')
+
+# 窓関数
+window = np.hanning(buffer_size)
+
+def callback(indata, frames, time, status):
+    if status:
+        print(status)
+    # print(indata.shape, frames, time)
+    data_windowed = indata[:, 0] * window
+    spectrum = np.fft.rfft(data_windowed, n=frames)
+    magnitude = np.abs(spectrum)*2 / frames
+    line.set_ydata(magnitude)
+    plt.draw()
+
+stream = sd.InputStream(callback=callback, device=device_num, channels=channels,
+                        samplerate=fs, blocksize=buffer_size, dtype='int16')
+with stream:
+    plt.show(block=True)
